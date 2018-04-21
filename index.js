b594dac268320f3d8728bd8c580067336d4083b2
Mihail Craciunescu <mihail.craciunescu@gmail.com>
2018-04-22 01:00:24 +0300

    test

---
+++ index.js
@@ -0,0 +1,20 @@
+const cool = require('cool-ascii-faces');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+.get('/', (req, res) => res.render('pages/index'))
+.get('/cool', function(request, response) {
+  response.send(cool());
+})
+.get('/times', function(request, response) {
+    var result = ''
+    var times = process.env.TIMES || 5
+    for (i=0; i < times; i++)
+      result += i + ' ';
+  response.send(result);
+}).listen(PORT, () => console.log(`Listening on ${ PORT }`))
